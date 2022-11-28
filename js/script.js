8e3ee2670d0a0519ba278558a721b2bb34a4342e
R5G1 <vlad.klimachevski@gmail.com>
2022-11-28 16:28:05 +0300

    feat: add glamurnenko projekt

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', e => {
+    const scroll = document.querySelector(".header-navigation")
+
+    if (pageYOffset > 500) {
+        scroll.classList.add('activ')
+    } if (pageYOffset < 500) {
+        scroll.classList.remove('activ')
+    }
+})
+
+
+console.log(scroll);
